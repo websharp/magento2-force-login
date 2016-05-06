abbe1d5e82edf053094d853ffa5b5a2e57180c4f
Florian Horn <f.horn@bitexpert.de>
2016-05-06 10:34:39 +0200

    Fixed grid loading

--- Block/Adminhtml/Whitelist.php
+++ Block/Adminhtml/Whitelist.php
@@ -17,26 +17,6 @@ namespace bitExpert\ForceCustomerLogin\Block\Adminhtml;
 class Whitelist extends \Magento\Backend\Block\Widget\Container
 {
     /**
-     * @var \Banc\AssetProduct\Api\Helper\Type\AssetTypeHelperInterface
-     */
-    protected $assetTypeHelper;
-
-    /**
-     * @param \Magento\Backend\Block\Widget\Context $context
-     * @param \Banc\AssetProduct\Api\Helper\Type\AssetTypeHelperInterface $assetTypeHelper
-     * @param array $data
-     */
-    public function __construct(
-        \Magento\Backend\Block\Widget\Context $context,
-        \Banc\AssetProduct\Api\Helper\Type\AssetTypeHelperInterface $assetTypeHelper,
-        array $data = []
-    ) {
-        $this->assetTypeHelper = $assetTypeHelper;
-
-        parent::__construct($context, $data);
-    }
-
-    /**
      * Prepare button and grid
      *
      * @return \Banc\AssetBuilder\Block\Adminhtml\Asset
@@ -44,12 +24,12 @@ class Whitelist extends \Magento\Backend\Block\Widget\Container
     protected function _prepareLayout()
     {
         $addButtonProps = [
-            'id' => 'add_new_asset',
-            'label' => __('Add Asset'),
-            'class' => 'add',
+            'id' => 'add_new_entry',
+            'label' => __('Add Entry'),
+            'class' => 'primary add',
             'button_class' => '',
-            'class_name' => 'Magento\Backend\Block\Widget\Button\SplitButton',
-            'options' => $this->_getAddAssetButtonOptions(),
+            'onclick' => "setLocation('" . $this->getCreateUrl() . "')",
+            'class_name' => 'Magento\Backend\Block\Widget\Button'
         ];
         $this->buttonList->add('add_new', $addButtonProps);
 
@@ -57,43 +37,14 @@ class Whitelist extends \Magento\Backend\Block\Widget\Container
     }
 
     /**
-     * Retrieve options for 'Add Asset' split button
+     * Retrieve create url
      *
-     * @return array
-     */
-    protected function _getAddAssetButtonOptions()
-    {
-        $splitButtonOptions = [];
-        $types = $this->assetTypeHelper->getTypes();
-        uasort(
-            $types,
-            function ($elementOne, $elementTwo) {
-                return ($elementOne['sort_order'] < $elementTwo['sort_order']) ? -1 : 1;
-            }
-        );
-
-        foreach ($types as $typeId => $type) {
-            $splitButtonOptions[$typeId] = [
-                'label' => __($type['label']),
-                'onclick' => "setLocation('" . $this->_getAssetCreateUrl($typeId) . "')",
-                'default' => \Banc\AssetProduct\Helper\Type\AssetTypeHelper::DEFAULT_TYPE === $typeId,
-            ];
-        }
-
-        return $splitButtonOptions;
-    }
-
-    /**
-     * Retrieve product create url by specified product type
-     *
-     * @param string $type
      * @return string
      */
-    protected function _getAssetCreateUrl($type)
+    protected function getCreateUrl()
     {
         return $this->getUrl(
-            'AssetBuilder/asset/create',
-            ['type' => $type]
+            'ForceCustomerLogin/Whitelist/Create'
         );
     }
 }
