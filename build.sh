e45b826f8853bc73c141207394a247861fbdd101
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2018-11-25 20:28:41 +0100

    Add "apt update" command in build script to make build not fail

--- build.sh
+++ build.sh
@@ -368,7 +368,7 @@ rm -rf "${DEST}"/*
 ## Update dependencies
 ## Based upon  PHP 7.0
 ##
-docker run -v "${SRC}":/www -v ~/.composer:/root/.composer danieldent/php-7.0-composer:latest bash -c 'apt-get install -y libfreetype6-dev libjpeg62-turbo-dev libmcrypt-dev libpng12-dev libxslt-dev && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ && docker-php-ext-install gd mcrypt xsl && cd /www && composer update'
+docker run -v "${SRC}":/www -v ~/.composer:/root/.composer danieldent/php-7.0-composer:latest bash -c 'apt-get update && apt-get install -y libfreetype6-dev libjpeg62-turbo-dev libmcrypt-dev libpng12-dev libxslt-dev && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ && docker-php-ext-install gd mcrypt xsl && cd /www && composer update'
 
 ##
 ## Prepare important files
